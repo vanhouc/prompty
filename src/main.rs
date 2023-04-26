93d1fbf2fcca4decfac9edf445e5093213ab3aaf
= <=>
2023-04-25 20:17:50 -0400

    refactored error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod openai;
 
 use openai::OpenAiError;
-use poise::serenity_prelude::{self as serenity, ChannelId, Message};
-use tokio::runtime::Runtime;
+use poise::{
+    command,
+    serenity_prelude::{self as serenity, Message},
+};
 use tracing::{error, info, instrument};
 use tracing_subscriber::{filter, prelude::*};
 
@@ -13,8 +15,7 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 /// Takes a text prompt and creates a lovely image
-#[instrument]
-#[poise::command(slash_command)]
+#[command(slash_command)]
 async fn paint(
     ctx: Context<'_>,
     #[description = "A text description for prompty to work off of"] description: String,
@@ -22,12 +23,25 @@ async fn paint(
     info!("Received paint request");
     // It can take some time for openai to respond so send a defferal to discord to give us more time
     ctx.defer().await?;
-    paint_internal(ctx, None, &description).await
+    info!("Submitting paint request to OpenAI");
+    match openai::get_openai_image(&description).await {
+        Ok(bytes) => {
+            info!("Received valid response from OpenAI");
+            let file = (&bytes[..], "ai_response.png");
+            ctx.send(|m| {
+                m.attachment(file.into())
+                    .embed(|e| e.title(&description).attachment("ai_response.png"))
+            })
+            .await?;
+            info!("Posted painting")
+        }
+        Err(error) => handle_openai_error(ctx, &error).await,
+    }
+    Ok(())
 }
 
 /// Draw an image describing this messages content
-#[instrument]
-#[poise::command(context_menu_command = "Draw Message")]
+#[command(context_menu_command = "Draw Message")]
 async fn paint_message(
     ctx: Context<'_>,
     #[description = "A message to draw an image from"] message: Message,
@@ -38,14 +52,27 @@ async fn paint_message(
         .channel_id
         .create_public_thread(ctx, &message, |f| f.name("Drawing"))
         .await?;
-    paint_internal(ctx, Some(thread.into()), &message.content).await?;
-    ctx.say("All done!!!").await?;
+    info!("Submitting paint request to OpenAI");
+    match openai::get_openai_image(&message.content).await {
+        Ok(bytes) => {
+            info!("Received valid response from OpenAI");
+            let file = (&bytes[..], "ai_response.png");
+            thread
+                .send_message(ctx, |m| {
+                    m.add_file(file)
+                        .embed(|e| e.title(&message.content).attachment("ai_response.png"))
+                })
+                .await?;
+            ctx.say("All done!!!").await?;
+            info!("Posted painting")
+        }
+        Err(error) => handle_openai_error(ctx, &error).await,
+    }
     Ok(())
 }
 
 /// Ask the bot a question
-#[instrument(skip(ctx))]
-#[poise::command(slash_command)]
+#[command(slash_command)]
 async fn ask(
     ctx: Context<'_>,
     #[description = "A question for the bot to answer"] question: String,
@@ -60,73 +87,35 @@ async fn ask(
                 .await?;
             info!("Posted answer")
         }
-        Err(error) => {
-            error!("{}", &error);
-            match error {
-                OpenAiError::Safety => {
-                    ctx.say("Bonk!!! Go directly to horny jail").await?;
-                }
-                OpenAiError::LimitReached => {
-                    ctx.say("Looks like I'm all out of paint this month :(")
-                        .await?;
-                }
-                _ => {
-                    ctx.say("Uh oh something went wrong while I was trying to respond!")
-                        .await?;
-                }
-            }
-        }
-    }
-    Ok(())
-}
-async fn paint_internal(
-    ctx: Context<'_>,
-    channel: Option<ChannelId>,
-    prompt: &str,
-) -> Result<(), Error> {
-    info!("Submitting paint request to OpenAI");
-    match openai::get_openai_image(prompt).await {
-        Ok(bytes) => {
-            info!("Received valid response from OpenAI");
-            let file = (&bytes[..], "ai_response.png");
-            if let Some(channel) = channel {
-                channel
-                    .send_message(ctx, |m| {
-                        m.add_file(file)
-                            .embed(|e| e.title(prompt).attachment("ai_response.png"))
-                    })
-                    .await?;
-            } else {
-                ctx.send(|m| {
-                    m.attachment(file.into())
-                        .embed(|e| e.title(prompt).attachment("ai_response.png"))
-                })
-                .await?;
-            }
-            info!("Posted painting")
-        }
-        Err(error) => {
-            sentry::capture_error(&error);
-            match error {
-                OpenAiError::Safety => {
-                    ctx.say("Bonk!!! Go directly to horny jail").await?;
-                }
-                OpenAiError::LimitReached => {
-                    ctx.say("Looks like I'm all out of paint this month :(")
-                        .await?;
-                }
-                _ => {
-                    ctx.say("Uh oh something went wrong while I was trying to respond!")
-                        .await?;
-                }
-            }
-        }
+        Err(error) => handle_openai_error(ctx, &error).await,
     }
     Ok(())
 }
 
+async fn handle_openai_error(ctx: Context<'_>, error: &OpenAiError) {
+    error!("OpenAi request resulted in error: {:?}", &error);
+    let result = match error {
+        OpenAiError::Safety => ctx.say("Bonk!!! Go directly to horny jail").await,
+        OpenAiError::LimitReached => {
+            ctx.say("Looks like I'm all out of paint this month :(")
+                .await
+        }
+        _ => {
+            ctx.say("Uh oh something went wrong while I was trying to respond!")
+                .await
+        }
+    };
+    if let Err(response_error) = result {
+        error!(
+            "Encountered error while trying to inform user of error: {:?}",
+            response_error
+        )
+    }
+}
+
+#[tokio::main]
 #[instrument]
-fn main() {
+async fn main() {
     dotenv::dotenv().ok();
 
     let _sentry = sentry::init((
@@ -161,7 +150,5 @@ fn main() {
             })
         });
 
-    let rt = Runtime::new().unwrap();
-
-    rt.block_on(framework.run()).unwrap();
+    framework.run().await.unwrap();
 }
